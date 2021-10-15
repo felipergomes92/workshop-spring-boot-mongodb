dffdb60e22476810b9b423b5ccfd982057ecf15c
Felipe Ramos <felipergomes92@gmail.com>
2021-10-15 18:26:25 -0300

    Consulta simples com query methods

--- src/main/java/com/felipegomes/workshopmongo/resources/PostResource.java
+++ src/main/java/com/felipegomes/workshopmongo/resources/PostResource.java
@@ -1,25 +1,36 @@
 package com.felipegomes.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipegomes.workshopmongo.domain.Post;
+import com.felipegomes.workshopmongo.resources.util.URl;
 import com.felipegomes.workshopmongo.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
-	
+
 	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value= "/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="")String text) {
+		text = URl.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ src/main/java/com/felipegomes/workshopmongo/resources/util/URl.java
@@ -0,0 +1,15 @@
+package com.felipegomes.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URl {
+
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
